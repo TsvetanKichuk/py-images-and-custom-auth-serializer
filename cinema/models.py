fbeca1be5c865ae578b5307a3468e2d6eecd5cd2
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-10-10 16:34:53 +0200

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -1,5 +1,5 @@
 import os
-import pathlib
+
 import uuid
 
 from django.core.exceptions import ValidationError
--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model, authenticate
 from django.utils.translation import gettext as _
+
 from rest_framework import serializers
 
 
